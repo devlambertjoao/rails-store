13d0665ece6fb7e69d17f0062f40e47d8bb1b5e5
Joao Lambert <devlambertjoao@gmail.com>
2023-03-12 23:35:22 -0300

    savepoint fixing tests

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,4 +1,8 @@
 class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  def self.destroy_by_product(product)
+    where(product_id: product.id).each { |order_product| order_product.delete }
+  end
 end
