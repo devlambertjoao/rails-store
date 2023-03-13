92021db3ec753e4b1691cdb64be9af3bb455e962
Joao Lambert <devlambertjoao@gmail.com>
2023-03-13 09:11:40 -0300

    Removing unnecessary

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,8 +1,4 @@
 class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
-
-  def self.destroy_by_product(product)
-    where(product_id: product.id).each { |order_product| order_product.delete }
-  end
 end
